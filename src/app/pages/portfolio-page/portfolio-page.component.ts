77e3a1ec00656b45350d810d2e2ff7c493e0b714
MarcoDevLagoon <marco.ramiandra@devlagoonsolutions.com>
2025-05-12 23:51:59 +0200

    first commit

---
+++ src/app/pages/portfolio-page/portfolio-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-portfolio-page',
+  imports: [],
+  templateUrl: './portfolio-page.component.html',
+  styleUrl: './portfolio-page.component.scss'
+})
+export class PortfolioPageComponent implements OnInit {
+  constructor(private title: Title, private meta: Meta) { }
+
+  ngOnInit(): void {
+    this.title.setTitle('Marco Ramiandraharivelo - Développeur Web Full Stack');
+    this.meta.addTags([
+      { name: 'description', content: 'Portfolio de Marco Tolotriniaina Ramiandraharivelo, développeur web full stack spécialisé en Angular et Node.js.' },
+      { name: 'robots', content: 'index, follow' }
+    ]);
+  }
+}
